433d638c96849c50baf342d2279c10733862a3e7
tayefulislam <kazitayefulislam12@gmail.com>
2024-08-21 13:24:13 +0600

    set bit done

--- Bits.java
+++ Bits.java
@@ -4,16 +4,27 @@ public class Bits {
 
     public static void main(String[] args) {
 
-        int n = 13;
-        int pos = 0;
-        int bitMask = 1 >> pos;
+        int n = 5;
+        int pos = 1;
+        int bitMask = 1 << pos;
 
-        if ((bitMask & n) == 0) {
-            System.out.println("Zero");
-            System.out.println(bitMask & n);
-        } else
-            System.out.println("One");
-        System.out.println(bitMask & n);
+        // Check Bit
+        /*
+         *
+         * if ((bitMask & n) == 0) {
+         * System.out.println("Zero");
+         * System.out.println(bitMask & n);
+         * } else
+         * System.out.println("One");
+         * System.out.println(bitMask & n);
+         * 
+         * 
+         */
+
+        // Set Bit
+
+        int newNum = bitMask | n;
+        System.out.println(newNum);
 
     }
 }
