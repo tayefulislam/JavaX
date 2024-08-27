341f5426516968a9e7a1f2914306488fadc53796
tayefulislam <kazitayefulislam12@gmail.com>
2024-08-28 01:35:45 +0600

    factorail added

--- Recursion.java
+++ Recursion.java
@@ -7,21 +7,33 @@ public class Recursion {
     // System.out.println(start);
     // printNumber(start + 1, n);
     // }
-    public static void printSum(int start, int n, int sum) {
+    // public static void printSum(int start, int n, int sum) {
 
-        if (n == start) { // Base Case
-            sum += start;
-            System.out.println("Total Sum : " + sum);
-            return;
+    // if (n == start) { // Base Case
+    // sum += start;
+    // System.out.println("Total Sum : " + sum);
+    // return;
+    // }
+
+    // sum += start;
+    // printSum(start + 1, n, sum);
+
+    // }
+    public static int printFactorial(int n) {
+
+        if (n == 1 || n == 1) {
+            return 1;
         }
 
-        sum += start;
-        printSum(start + 1, n, sum);
-
+        int calfact_nm1 = printFactorial(n - 1);
+        int cal_total = n * calfact_nm1;
+        return cal_total;
     }
 
     public static void main(String arg[]) {
         // printNumber(1, 10);
-        printSum(1, 5, 0);
+        // printSum(1, 5, 0);
+        System.out.println(printFactorial(5));
+
     }
 }
