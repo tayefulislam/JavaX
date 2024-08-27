3d04d2ab60782f132ebacb5c6bc47cfef43f486a
tayefulislam <kazitayefulislam12@gmail.com>
2024-08-28 00:51:21 +0600

    Recursion - Print Sum Added

--- Recursion.java
+++ Recursion.java
@@ -1,14 +1,27 @@
 public class Recursion {
 
-    public static void printNumber(int start, int n) {
-        if (start == n + 1) { // Base Case
+    // public static void printNumber(int start, int n) {
+    // if (start == n + 1) { // Base Case
+    // return;
+    // }
+    // System.out.println(start);
+    // printNumber(start + 1, n);
+    // }
+    public static void printSum(int start, int n, int sum) {
+
+        if (n == start) { // Base Case
+            sum += start;
+            System.out.println("Total Sum : " + sum);
             return;
         }
-        System.out.println(start);
-        printNumber(start + 1, n);
+
+        sum += start;
+        printSum(start + 1, n, sum);
+
     }
 
     public static void main(String arg[]) {
-        printNumber(1, 10);
+        // printNumber(1, 10);
+        printSum(1, 5, 0);
     }
 }
